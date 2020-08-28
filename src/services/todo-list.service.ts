4546838e27587cb4bce94b8299ac07caa443e22a
BlackOut <benlama.amine@gmail.com>
2020-08-28 13:32:00 +0200

    all required methods were created into todo list service file

--- src/services/todo-list.service.ts
+++ src/services/todo-list.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database'
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoListService {
 
-  constructor() { }
+  todoList: AngularFireList<any>;
+
+  constructor(private firebaseDb: AngularFireDatabase) { }
+
+  getTodoList(){
+      this.todoList = this.firebaseDb.list('titles');
+      return this.todoList;
+  }
+
+  addTask(title: string) {
+    this.todoList.push({
+      title: title,
+      isChecked: false
+    })
+  }
+
+  checkOrUnCheckTask($key: string, flag: boolean) {
+    this.todoList.update($key, { isChecked: flag })
+  }
+
+  removeTask($key: string) {
+    this.todoList.remove($key);
+  }
 }
