31eec10b7c78d20babcd85327375a41ead63d074
BlackOut <benlama.amine@gmail.com>
2020-08-28 13:28:57 +0200

    firebase was configured & integrated to My todoApp project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  // Your web app's Firebase configuration
+  firebaseConfig : {
+    apiKey: "AIzaSyDtNpZGH1pylDahxyqLbW_uZbjdTGdWASE",
+    authDomain: "angulartodoapp-9bdca.firebaseapp.com",
+    databaseURL: "https://angulartodoapp-9bdca.firebaseio.com",
+    projectId: "angulartodoapp-9bdca",
+    storageBucket: "angulartodoapp-9bdca.appspot.com",
+    messagingSenderId: "1038504843846",
+    appId: "1:1038504843846:web:cbe6712fbb7e7caa6a8847",
+    measurementId: "G-BF26QFYWGB"
+  }
 };
 
 /*
